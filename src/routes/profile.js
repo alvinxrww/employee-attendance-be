5420d38d1a4549200dd11b0010cb3310da9c5066
alvinxrww <alvin.xavier@ui.ac.id>
2025-01-26 07:01:29 +0700

    feat: get user profile

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -7,6 +7,29 @@ const path = require('path');
 
 const router = express.Router();
 
+// Get user profile
+router.get('/', authenticate, async (req, res) => {
+    try {
+        const { id } = req.user;
+
+        const user = await User.findByPk(id, {
+            attributes: {
+                exclude: ['password'], // Exclude password from the result
+            },
+        });
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        res.status(200).json({ user });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+
+// Update user profile
 router.put('/', authenticate, upload.single('profile_photo'), async (req, res) => {
     try {
         const { id } = req.user;
